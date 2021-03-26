d836d36b59c73db758d8b40bb7423a8949cc7d7a
Devin M. O'Brien <d_obrien@uncg.edu>
2021-03-25 20:44:16 -0400

    Displaying Pieces This commit adds some functionality to pieces so they now can load an image corresponding to their piece; right now this is the svg set I uploaded. Wee need some tweaking, but it is displays the pawn.

--- src/chess/cell/cell.component.ts
+++ src/chess/cell/cell.component.ts
@@ -1,6 +1,6 @@
 
 import { Component, OnInit, Input } from '@angular/core';
-
+import { PieceComponent } from '../piece/piece.component';
 @Component({
   selector: 'app-cell',
   templateUrl: './cell.component.html',
@@ -20,6 +20,7 @@ export class CellComponent implements OnInit {
   @Input("init")
   style:string = "";
   
+  piece:PieceComponent = new PieceComponent();
   
   constructor() { }
 
--- src/chess/piece/piece.component.ts
+++ src/chess/piece/piece.component.ts
@@ -8,6 +8,37 @@ import { Types } from "./types.enum"
 export class PieceComponent implements OnInit {
   type:Types = Types.BlackPawn;
 
+  getImage(){
+        switch(this.type){
+            case Types.BlackPawn:
+                return "Chess_pdt45.svg";
+            case Types.WhitePawn:
+                return "Chess_plt45.svg";
+            case Types.BlackKnight:
+                return "Chess_ndt45.svg";
+            case Types.WhiteKnight:
+                return "Chess_nlt45.svg";
+            case Types.WhiteBishop:
+                return "Chess_blt45.svg";
+            case Types.BlackBishop:
+                return "Chess_bdt45.svg";
+            case Types.BlackQueen:
+                return "Chess_qdt45.svg";
+            case Types.WhiteQueen:
+                return "Chess_qlt45.svg";
+            case Types.WhiteKing:
+                return "Chess_klt45.svg";
+            case Types.BlackKing:
+                return "Chess_kdt45.svg";
+            case Types.BlackRook:
+                return "Chess_rdt45.svg";
+            case Types.WhiteRook:
+                return "Chess_rlt45.svg";
+            default:
+                return "NONE";
+        }
+  }
+  
   constructor() { }
 
   ngOnInit(): void {
