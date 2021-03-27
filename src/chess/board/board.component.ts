15cc8440c8329357235d9c24598782203c99f9e3
Devin M. O'Brien <d_obrien@uncg.edu>
2021-03-27 14:06:12 -0400

    Improved Testing Button This is just something to show who the board in action, however a quick note is that the rows may need to be reset before generating a new board for example in the memo function (it is what the button calls), there are two board generation functions that will be called on the 2nd and 3rd click respectively. The 2nd click works without resetting the rows while 3rd click had to be regenerrated to display properly. This is probably something I need to add to the generation function.

--- src/chess/board/board.component.ts
+++ src/chess/board/board.component.ts
@@ -37,29 +37,30 @@ export class BoardComponent implements OnInit {
 	 * Returns a row with the first cell having primary style.
 	 *  TODO: Add Counter For Digits
 	 */
-	addRow(classA: string, classB: string, fen:string) {
+	addRow(classA: string, classB: string, fen: string) {
 		let z: number = 8;
 		let y: number = 0;
 		let temp: Row = new Row();
-		let counter = 0;		
-		let counter2 = 0;		
+		let counter = 0;
+		let counter2 = 0;
 		for (; y < z; y++) {
 			let fs = fen.charAt(counter);
 			console.log(fs)
-			if (!isNaN(Number(fs))){
-				if(counter2 == 0){
-						counter2++;			
-				}if(counter2 == Number(fs)){
-						counter2 = 0;
-						counter++;	
-					}else{
-						counter2++;	
-					}			
-							
-			}else{
+			if (!isNaN(Number(fs))) {
+				if (counter2 == 0) {
+					counter2++;
+				}
+				if (counter2 == Number(fs)) {
+					counter2 = 0;
+					counter++;
+				} else {
+					counter2++;
+				}
+
+			} else {
 				console.log("TEST");
 				console.log(counter);
-				counter++;	
+				counter++;
 			}
 			let a = new CellComponent();
 			if (y % 2 == 0) {
@@ -69,25 +70,34 @@ export class BoardComponent implements OnInit {
 			}
 			console.log(Number(fs))
 			console.log(counter)
-			a.setFEN((isNaN(Number(fs))?fs:""))
+			a.setFEN((isNaN(Number(fs)) ? fs : ""))
 			temp.addCell(a);
 		}
 		return temp;
 	}
-  test_counter:number = 0;
-	
-	memo(){
-	if(this.test_counter == 0){
-		this.move(0,1,0,2);
-		this.test_counter++;	
-	}else{
-	this.rows = [];
-	this.generateBoard("bg-primary", "bg-secondary", "r1b1k1nr/p2p1pNp/n2B4/1p1NP2P/6P1/3P1Q2/P1P1K3/q5b1");
-	this.printFENString();
-}
+	test_counter: number = 0;
+
+	memo() {
+		if (this.test_counter == 0) {
+			this.move(0, 1, 0, 2);
+			this.test_counter++;
+		} else {
+			if (this.test_counter == 1) {
+				this.rows = [];
+				this.test_counter = 3;
+				this.generateBoard("bg-primary", "bg-secondary", "r1b1k1nr/p2p1pNp/n2B4/1p1NP2P/6P1/3P1Q2/P1P1K3/q5b1");
+				this.printFENString();
+			} else {
+				if (this.test_counter == 3) {
+					this.rows = []
+					this.generateBoard("bg-primary", "bg-secondary");
+					this.test_counter = 0;
+				}
+			}
+		}
 	}
-	
-	move(x1:number, y1:number, x2:number, y2:number){
+
+	move(x1: number, y1: number, x2: number, y2: number) {
 		let temp = this.rows[y1].cells[x1].toFENString();
 		this.rows[y1].cells[x1].setFEN("");
 		this.rows[y2].cells[x2].setFEN(temp);
@@ -104,7 +114,7 @@ export class BoardComponent implements OnInit {
 	 *
 	 * Creates the chessboard with alternating rows.
 	 */
-	generateBoard(classA: string, classB: string, startingPos="rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR") {
+	generateBoard(classA: string, classB: string, startingPos = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR") {
 		let num: number = 8;
 		let counter: number = 0;
 		let fen = startingPos.split("/")
@@ -135,8 +145,8 @@ export class BoardComponent implements OnInit {
 		let output = "";
 
 		for (; i < length; i++) {
-			
-			output += this.rows[i].toFENString() + (i+1==length?"":"/");
+
+			output += this.rows[i].toFENString() + (i + 1 == length ? "" : "/");
 		}
 		return output;
 	}
