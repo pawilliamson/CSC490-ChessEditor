fd354bf011a76021bd50a1847d5d150793e35d50
TylerNW3994 <TylerNW3994@gmail.com>
2021-04-07 16:39:50 -0400

    Added board testing

--- src/chess/board/board.component.spec.ts
+++ src/chess/board/board.component.spec.ts
@@ -22,9 +22,29 @@ describe('BoardComponent', () => {
     fixture.detectChanges();
   });
   
-  it('should generate normal chessboard', () => {
-    const initial_layout = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR";
-    const result = component.toFENString();
-    expect(result).toContain(initial_layout);
+  describe('Initialization', function() {
+    it('should be defined', () => {
+      expect(component).toBeDefined();
+    });
+  });
+
+  describe('Create Board', function(){
+    it('should generate normal chessboard', () => {
+      const initial_layout = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR";
+      const result = component.toFENString();
+      expect(result).toContain(initial_layout);
+    });
+
+    it('should generate an empty chessboard', () => {
+      const initialLayout = "8/8/8/8/8/8/8/8";
+      const result = component.toFENString();
+      expect(result).toContain(initialLayout);
+    });
+
+    it('should generate an empty board with an empty layout', () =>{
+      const initialLayout = "";
+      const result = component.toFENString();
+      expect(result).toContain(initialLayout);
+    });
   });
 });
