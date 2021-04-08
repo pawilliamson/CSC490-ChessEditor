07e4e96e98efe3d2b305d322bd830be0f634ddb3
Dakoats <simpkinsdakota@gmail.com>
2021-04-07 22:42:53 -0400

    Adjusted location of buttons

---
+++ src/chess/home/home.component.spec.ts
@@ -0,0 +1,29 @@
+/* File: home.component.spec.ts
+	File containing tests for Board Component.
+*/
+
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { HomeComponent } from './home.component';
+
+describe('HomeComponent', () => {
+    let component: HomeComponent;
+    let fixture: ComponentFixture<HomeComponent>;
+
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [HomeComponent]
+        })
+        .compileComponents();
+    });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(HomeComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should generate home page', () => {
+        //To fill with test cases
+    });
+
+});
